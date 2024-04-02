a9adecc351bfef0105649d45f93f9906d1b62f9b
giraffenostrils <dreamthre@gamil.com>
2024-04-02 16:06:49 +0900

    tailwind

---
+++ day9/vite-project/src/page/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import ViewCount from "../components/ViewCount";
+import ChangeCount from "../components/CangeCount";
+import LogCount from "../components/LogCount";
+
+
+ 
+  const Counter = () => {
+  
+    const [logs, setLogs] = useState([]);
+  
+    return (
+      <div className="container">
+        <ViewCount />
+        <ChangeCount />
+        <hr className="m-4" />
+        <LogCount logs={logs} />
+      </div>
+    );
+  };
+  
+
+export default Counter;
